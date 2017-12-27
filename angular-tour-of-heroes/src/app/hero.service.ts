3c13d1227e62f976d46b79c81556010c6851549d
Richard Zhang <richard@fiitfu.com>
2017-12-27 15:12:58 -0800

    sort heroes

--- angular-tour-of-heroes/src/app/hero.service.ts
+++ angular-tour-of-heroes/src/app/hero.service.ts
@@ -2,12 +2,18 @@ import { Injectable } from '@angular/core';
 import { Hero } from './hero';
 import { HEROES } from './bnha_heroes';
 
+import { Observable } from 'rxjs/Observable';
+import { of } from 'rxjs/observable/of';
+
 @Injectable()
 
 export class HeroService {
 
-	getHeroes(): Hero[] {
-		return HEROES;
+	getHeroes(): Observable<Hero[]> {
+		let sortedHEROES = HEROES.sort((a,b) => {
+			return a.id - b.id;
+		});
+	  return of(sortedHEROES);
 	}
 
   constructor() { }
