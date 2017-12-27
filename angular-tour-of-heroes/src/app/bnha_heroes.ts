4851908c122049d7a8657ae15589dc5223b0d13a
Richard Zhang <richard@fiitfu.com>
2017-12-27 12:53:22 -0800

    add hero list

---
+++ angular-tour-of-heroes/src/app/bnha_heroes.ts
@@ -0,0 +1,11 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+	{ id: 1, name: 'All Might'},
+	{ id: 2, name: 'Endeavour'},
+	{ id: 32, name: 'Rag Doll'},
+	{ id: 4, name: 'Best Jeanist'},
+	{ id: 9, name: 'Dragoon Hero "Ryuku"'},
+	{ id: 5, name: 'Edgeshot'},
+	{ id: 10, name: 'Gang Orca'}
+];
