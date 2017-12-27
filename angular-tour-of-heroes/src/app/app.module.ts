afa933c5d7a3328e550fa60a4738efb4a07c9679
Richard Zhang <richard@fiitfu.com>
2017-12-27 15:44:49 -0800

    add message service and component

--- angular-tour-of-heroes/src/app/app.module.ts
+++ angular-tour-of-heroes/src/app/app.module.ts
@@ -6,21 +6,25 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { MessageService } from './message.service.ts';
 
 import { HeroService } from './hero.service';
+import { MessagesComponent } from './messages/messages.component';
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroesComponent,
-    HeroDetailComponent
+    HeroDetailComponent,
+    MessagesComponent
   ],
 	imports: [
 	  BrowserModule,
 	  FormsModule
 	],
-	providers: [ HeroService ],
+	providers: [ HeroService, MessageService ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- angular-tour-of-heroes/src/app/hero.service.ts
+++ angular-tour-of-heroes/src/app/hero.service.ts
@@ -1,21 +1,23 @@
 import { Injectable } from '@angular/core';
-import { Hero } from './hero';
-import { HEROES } from './bnha_heroes';
 
 import { Observable } from 'rxjs/Observable';
 import { of } from 'rxjs/observable/of';
 
-@Injectable()
+import { Hero } from './hero';
+import { HEROES } from './bnha_heroes';
+import { MessageService } from './message.service.ts';
 
+@Injectable()
 export class HeroService {
 
 	getHeroes(): Observable<Hero[]> {
 		let sortedHEROES = HEROES.sort((a,b) => {
 			return a.id - b.id;
 		});
+		 this.messageService.add('HeroService: fetched heroes');
+		 console.log(this.messageService);
 	  return of(sortedHEROES);
 	}
 
-  constructor() { }
-
+	constructor(private messageService: MessageService) { }
 }
