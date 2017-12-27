ffd76f88ba412927448ed54aae18102a64f2fc54
Richard Zhang <richard@fiitfu.com>
2017-12-27 12:53:31 -0800

    display list of heroes

--- angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,15 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
+import { HEROES } from '../bnha_heroes.ts';
+
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css'],
 })
 export class HeroesComponent implements OnInit {
-	hero: Hero = {
-		id: 1,
-		name: 'Sans'
-	};
+  heroes = HEROES;
 
   constructor() {}
 
