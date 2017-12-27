01c50ebb7e810b0e99f5650222b9d90a0a4c2a55
Richard Zhang <richard@fiitfu.com>
2017-12-27 12:07:18 -0800

    show hero

--- angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -3,11 +3,12 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.css']
+  styleUrls: ['./heroes.component.css'],
 })
 export class HeroesComponent implements OnInit {
+	hero = 'Sans';
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
   }
